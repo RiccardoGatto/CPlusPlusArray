7a764646666750bb841701d43766104e0727c774
RiccardoGatto <gatto.riccardo@gmail.com>
2023-03-01 12:42:52 +0100

    Modifiche

--- es6.cpp
+++ es6.cpp
@@ -2,6 +2,23 @@
 #include <iostream>
 using namespace std;
 
+void riempi(int arr[], int lung)
+{
+    for (int i = 0; i < lung; i++) // RIEMPIO L'ARRAY
+    {
+        cout << "Ora inserisci il " << i + 1 << " numero: ";
+        cin >> arr[i];
+    }
+}
+
+void mostra(int arr[], int lung)
+{
+    for (int i = 0; i < lung; i++)
+    {
+        cout << i + 1 << " elemento: " << arr[i] << endl;
+    }
+}
+
 int contaPari(int arr[], int lung)
 {
     int totPari = 0;
@@ -14,21 +31,7 @@ int contaPari(int arr[], int lung)
     }
     return totPari;
 }
-void riempi(int arr[], int lung)
-{
-    for (int i = 0; i < lung; i++) //RIEMPIO L'ARRAY
-    {
-        cout << "Ora inserisci il " << i + 1 << " numero: ";
-        cin >> arr[i];
-    }
-}
-void mostra(int arr[], int lung)
-{
-    for (int i = 0; i < lung; i++)
-    {
-        cout << i+1 << " elemento: " << arr[i] << endl;
-    }
-}
+
 int main()
 {
     int quanti;
@@ -36,16 +39,16 @@ int main()
     cin >> quanti;
     cout << endl;
     int interi[quanti];
-    riempi (interi, quanti);
+    riempi(interi, quanti);
     int lunghezzaOutput = contaPari(interi, quanti); // CONTO QUANTI PARI CI SONO NELL'ARRAY CHE HO RIEMPITO
-    int pari[lunghezzaOutput]; // DEFINISCO UN ARRAY DI LUNGHEZZA QUANTO IL NUMERO DI PARI PRIMA DEFINITO
-    int index = 0; // INIZIALIZZO UN INDICE CHE MI SCORRE L'ARRAY DEI RISULTATI
-    for (int i = 0; i < quanti; i++) // SCORRO L'ARRAY ALLA RICERCA DEGLI ELEMENTI PARI
+    int pari[lunghezzaOutput];                       // DEFINISCO UN ARRAY DI LUNGHEZZA QUANTO IL NUMERO DI PARI PRIMA DEFINITO
+    int index = 0;                                   // INIZIALIZZO UN INDICE CHE MI SCORRE L'ARRAY DEI RISULTATI
+    for (int i = 0; i < quanti; i++)                 // SCORRO L'ARRAY ALLA RICERCA DEGLI ELEMENTI PARI
     {
         if (interi[i] % 2 == 0)
         {
             pari[index] = interi[i]; // SE E' PARI LO AGGIUNGO ALL'ARRAY DEI RISULTATI
-            index++; // INCREMENTO L'INDICE DELL'ARRAY RISULTATI
+            index++;                 // INCREMENTO L'INDICE DELL'ARRAY RISULTATI
         }
     }
     cout << "\n________________________________________________________\n\n";
